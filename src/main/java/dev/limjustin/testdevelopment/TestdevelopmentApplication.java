3af3e9fe71f2e7c241d6ca88b3c13ed5821d2756
Jaeyoung Lim <limjyjustin@gmail.com>
2024-04-26 16:46:14 +0900

    Add files via upload

---
+++ src/main/java/dev/limjustin/testdevelopment/TestdevelopmentApplication.java
@@ -0,0 +1,13 @@
+package dev.limjustin.testdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/limjustin/testdevelopment/TestdevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.limjustin.testdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
